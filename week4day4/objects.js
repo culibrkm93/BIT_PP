e6bbed9d3fd11ee3201b492f819b9f12de5ab83f
BIT Student <classroom@bgit.rs>
2019-05-16 14:15:00 +0200

    vezbe

---
+++ week4day4/objects.js
@@ -0,0 +1,14 @@
+var favoriteCoffee = {
+    name: 'Cappucino',
+    strength: 'medium',
+    flavour: 'mmm',
+    milk: '50%',
+    sugar: 'medium',
+}
+var favoriteMovie = {
+    title: 'The Huntsman',
+    actors: 'Emily Blunt',
+    genre: 'sci-fi',
+    popularity: '4',
+}
+
